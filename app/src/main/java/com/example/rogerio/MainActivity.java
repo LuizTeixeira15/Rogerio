c2aaea2dd63e506952f3a0e81befb93f49db87f9
Luiz Teixeira <luiz_ecia@hotmail.com>
2019-04-10 01:48:55 -0300

    4 commit

--- app/src/main/java/com/example/rogerio/MainActivity.java
+++ app/src/main/java/com/example/rogerio/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.rogerio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bt_tour, bt_bar;
+
+        bt_bar = (Button) findViewById(R.id.bt_bar);
+        bt_bar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+               startActivity(intent);
+
+            }
+        });
+
+
+
     }
 }
